c42fa26c418ed94f08d3da9e7df66ffa302f901c
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-06-07 22:52:48 +0300

    Added connection to MySql

--- src/main/java/com/rustret/worldguard/WorldGuard.java
+++ src/main/java/com/rustret/worldguard/WorldGuard.java
@@ -1,12 +1,25 @@
 package com.rustret.worldguard;
 
 import cn.nukkit.plugin.PluginBase;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 import com.rustret.worldguard.commands.*;
 import com.rustret.worldguard.listeners.*;
+
+import java.sql.SQLException;
 
 public class WorldGuard extends PluginBase {
     @Override
     public void onEnable() {
+        try {
+            ConnectionSource connectionSource = new JdbcConnectionSource("jdbc:mysql://localhost/worldguard?user=root&password=Pa$$w0rd&serverTimezone=UTC");
+            TableUtils.createTableIfNotExists(connectionSource, Region.class);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         getServer().getCommandMap().register("wand", new WandCommand());
         getServer().getCommandMap().register("rg", new RgCommand());
 
